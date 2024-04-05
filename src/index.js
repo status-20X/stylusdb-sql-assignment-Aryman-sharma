842e30de58a2b6528841f3feb3decee1243153e4
Aryman Sharma <aryman.sharma125@gmail.com>
2024-04-05 16:50:35 +0530

    Seventh Test Successful

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
+const parseQuery = require("./queryParser");
+const readCSV = require("./csvReader");
+
+function evaluateCondition(row, clause) {
+  const { field, operator, value } = clause;
+  switch (operator) {
+    case "=":
+      return row[field] === value;
+    case "!=":
+      return row[field] !== value;
+    case ">":
+      return row[field] > value;
+    case "<":
+      return row[field] < value;
+    case ">=":
+      return row[field] >= value;
+    case "<=":
+      return row[field] <= value;
+    default:
+      throw new Error(`Unsupported operator: ${operator}`);
+  }
+}
 
 async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
+  const { fields, table, whereClauses } = parseQuery(query);
 
-    // Apply WHERE clause filtering
-    const filteredData = whereClauses.length > 0
-        ? data.filter(row => whereClauses.every(clause => {
-            // You can expand this to handle different operators
-            return row[clause.field] === clause.value;
-        }))
-        : data;
+  const data = await readCSV(`${table}.csv`);
 
-    // Select the specified fields
-    return filteredData.map(row => {
-        const selectedRow = {};
-        fields.forEach(field => {
-            selectedRow[field] = row[field];
-        });
-        return selectedRow;
+  const filteredData = whereClauses.length > 0
+  ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
+  : data;
+
+  return filteredData.map((row) => {
+    const selectedRow = {};
+    fields.forEach((field) => {
+      selectedRow[field] = row[field];
     });
+    return selectedRow;
+  });
 }
 
 module.exports = executeSELECTQuery;
